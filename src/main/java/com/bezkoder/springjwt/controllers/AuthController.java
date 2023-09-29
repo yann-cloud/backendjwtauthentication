ad7ebef291ebb618761586e53d7e0c8fc604814e
yann-cloud <obamayann9@gmail.com>
2023-09-29 04:29:29 +0200

    test

--- src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
@@ -33,6 +33,7 @@ import com.bezkoder.springjwt.security.jwt.JwtUtils;
 import com.bezkoder.springjwt.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
+//maxAge : duree pendant laquelle la reponse est fraiche
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
---
+++ src/main/java/com/bezkoder/springjwt/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.bezkoder.springjwt.controllers;
+
+public class UserController {
+
+	public UserController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
